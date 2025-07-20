98a5ef1fbd4444cac864739075a49e5a7eda1fb8
Ninad Gawade <ninadgawade145@gmail.com>
2025-07-21 01:36:24 +0530

    New commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Home from './Home/Home'
 import { Routes, Route } from 'react-router-dom'
 import Collections from './Collections/Collections'
+import Signup from './components/Signup'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/collection' element={<Collections/>}/>
+      <Route path='/Signup' element={<Signup/>}/>
     </Routes>
     </>
   )
--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -1,12 +1,24 @@
-import React from "react";
-import {Link} from 'react-router-dom';
+import React from 'react'
+import {Link} from 'react-router-dom'
+import Login from './Login'
 
-const Login = () => {
+const Signup = () => {
   return (
     <>
-      <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
-        <div className="modal-box">
-          <h3 className="font-bold text-lg">Login</h3>
+    <div className='flex h-screen items-center justify-center'>
+    <div className="w-[500px] p-8 rounded-md shadow-2xl">
+        <div>
+          <h3 className="font-bold text-lg">Sign Up</h3>
+          {/* Name */}
+          <div className="mt-4 space-y-2">
+            <span>Name</span>
+            <br />
+            <input
+              type="text"
+              placeholder="Enter your Full Name"
+              className="w-80 px-3 py-2 rounded-md outline-none"
+            />
+          </div>
           {/* Email */}
           <div className="mt-4 space-y-2">
             <span>Email</span>
@@ -28,30 +40,32 @@ const Login = () => {
             />
           </div>
           {/* Button */}
-          <div className="flex justify-between mt-4">
+          <div className="flex justify-between mt-7">
             <button className="bg-black text-white rounded-md px-3 py-1 hover:bg-gray-400 duration-300 cursor-pointer">
-              Login
+              Sign Up
             </button>
             <p>
-              Not Registered ?{" "}
-              <Link
-                to={"/Signup"}
+              Have Account ?{" "}
+              <button
                 className="underline text-blue-500 cursor-pointer"
+                onClick={() => document.getElementById("my_modal_5").showModal()}
               >
-                Sign Up{" "}
-              </Link>
+                Login{" "}
+              </button>
+              <Login />
             </p>
           </div>
           <div className="modal-action">
             <form method="dialog">
               {/* if there is a button in form, it will close the modal */}
-              <button className="btn">Close</button>
+              <Link to={'/'} className="btn">Close</Link >
             </form>
           </div>
         </div>
-      </dialog>
+      </div>
+      </div>
     </>
-  );
-};
+  )
+}
 
-export default Login;
+export default Signup
